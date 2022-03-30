16f8f169976ba3c3c13181adf8238e8c9479f39d
Lucas Munhoz <lucasmunhozarruda@gmail.com>
2022-03-30 18:23:56 -0300

    Category Repository

---
+++ src/modules/cars/repositories/implementations/CategoryRepository.ts
@@ -0,0 +1,44 @@
+import { Category } from "../../model/Category";
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from "../ICategoriesRepository";
+
+export class CategoriesRepository implements ICategoriesRepository {
+  private categories: Category[];
+
+  // eslint-disable-next-line no-use-before-define
+  private static INSTANCE: CategoriesRepository;
+
+  private constructor() {
+    this.categories = [];
+  }
+
+  public static getInstance(): CategoriesRepository {
+    if (!CategoriesRepository.INSTANCE) {
+      CategoriesRepository.INSTANCE = new CategoriesRepository();
+    }
+
+    return CategoriesRepository.INSTANCE;
+  }
+
+  create({ name, description }: ICreateCategoryDTO): void {
+    const category = new Category({
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.categories.push(category);
+  }
+
+  list(): Category[] {
+    return this.categories;
+  }
+
+  findByName(name: string): Category {
+    const category = this.categories.find((category) => category.name === name);
+
+    return category;
+  }
+}

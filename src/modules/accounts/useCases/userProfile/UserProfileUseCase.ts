94311d015e394aadd4d4368acff7b3a8727e52c5
Lucas Munhoz <lucasmunhozarruda@gmail.com>
2022-06-02 21:29:27 -0300

    User Profile Use Case

---
+++ src/modules/accounts/useCases/userProfile/UserProfileUseCase.ts
@@ -0,0 +1,19 @@
+import { inject, injectable } from "tsyringe";
+
+import { IUserResponseDTO } from "@modules/accounts/dtos/IUserResponseDTO";
+import { UserMap } from "@modules/accounts/mapper/UserMap";
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+
+@injectable()
+export class UserProfileUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute(user_id: string): Promise<IUserResponseDTO> {
+    const user = await this.usersRepository.findById(user_id);
+
+    return UserMap.toDTO(user);
+  }
+}

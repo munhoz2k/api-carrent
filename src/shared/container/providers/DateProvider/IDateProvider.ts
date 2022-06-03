72ba311f5758c40237216bdfa34a94bdc275c4ae
Lucas Munhoz <lucasmunhozarruda@gmail.com>
2022-06-02 21:35:50 -0300

    Date Functions Interfcae

--- src/shared/container/providers/DateProvider/IDateProvider.ts
+++ src/shared/container/providers/DateProvider/IDateProvider.ts
@@ -2,5 +2,8 @@ export interface IDateProvider {
   compareInHours(end_date: Date, start_date: Date): number;
   convertToUTC(date: Date): string;
   dateNow(): Date;
-  compareInDays(end_date: Date, start_date: Date);
+  compareInDays(end_date: Date, start_date: Date): number;
+  addDays(days: number): Date;
+  addHours(hours: number): Date;
+  compareIfExpired(now_date: Date, target_date: Date): boolean;
 }

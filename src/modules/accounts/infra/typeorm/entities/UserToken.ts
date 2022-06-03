71e9ea670cef637f0f1078e1a5fb7c6281aedca7
Lucas Munhoz <lucasmunhozarruda@gmail.com>
2022-06-02 21:18:33 -0300

    User Refresh Tokens Entity

---
+++ src/modules/accounts/infra/typeorm/entities/UserToken.ts
@@ -0,0 +1,37 @@
+import { randomUUID } from "crypto";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+} from "typeorm";
+
+import { User } from "./User";
+
+@Entity("users_tokens")
+export class UserToken {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  refresh_token: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: "user_id" })
+  @Column()
+  user_id: string;
+
+  @Column()
+  expires_date: Date;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = randomUUID();
+    }
+  }
+}

9e50f7897d510cf8664a81fb0108200043c96afe
Lucas Munhoz <lucasmunhozarruda@gmail.com>
2022-06-02 21:27:55 -0300

    Auth Your Refresh Token Controller

---
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { RefreshTokenUseCase } from "./RefreshTokenUseCase";
+
+export class RefreshTokenController {
+  async handle(req: Request, res: Response): Promise<Response> {
+    const token =
+      req.body.token || req.headers["x-acess-token"] || req.query.token;
+
+    const refreshTokenUseCase = container.resolve(RefreshTokenUseCase);
+
+    const refresh_token = await refreshTokenUseCase.execute(token);
+
+    return res.status(200).json(refresh_token);
+  }
+}

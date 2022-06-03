0bd8d87267e5a9b6606480bff4d06594bafbf552
Lucas Munhoz <lucasmunhozarruda@gmail.com>
2022-06-02 21:42:43 -0300

    File Delete Util

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,12 +1,3 @@
-import fs from "fs";
+// import fs from "fs";
 
-export const deleteFile = async (filename: string) => {
-  await fs.promises
-    .stat(filename)
-    .then(async () => {
-      await fs.promises.unlink(filename);
-    })
-    .catch(() => {
-      console.log("Nada encontrado");
-    });
-};
+// export const deleteFile = async (filename: string) => {};

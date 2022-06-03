5b97185a4d34ccfb6a720344922c63d6f27085ae
Lucas Munhoz <lucasmunhozarruda@gmail.com>
2022-06-02 21:16:34 -0300

    Authentication Constants

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret: "b25cff77f9bad60a73fa81daa7d06ae",
+  secretRefresh: "o37m4ct9081asfg4g890u34j8079f",
+  expires_in_token: "15m",
+  expires_in_refresh_token: "30d",
+  refresh_token_days: 30,
+};

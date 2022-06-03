05a17832ed50a69c655d69940e42d2ffdb7dc724
Lucas Munhoz <lucasmunhozarruda@gmail.com>
2022-06-02 21:35:40 -0300

    Date Functions With Day.js

--- src/shared/container/providers/DateProvider/implementations/DayjsDateProvide.ts
+++ src/shared/container/providers/DateProvider/implementations/DayjsDateProvide.ts
@@ -21,10 +21,22 @@ export class DayjsDateProvider implements IDateProvider {
     return dayjs().toDate();
   }
 
-  compareInDays(end_date: Date, start_date: Date) {
+  compareInDays(end_date: Date, start_date: Date): number {
     const end_date_utc = this.convertToUTC(end_date);
     const start_date_utc = this.convertToUTC(start_date);
 
     return dayjs(end_date_utc).diff(start_date_utc, "days");
+  }
+
+  addDays(days: number): Date {
+    return dayjs().add(days, "days").toDate();
+  }
+
+  addHours(hours: number): Date {
+    return dayjs().add(hours, "hours").toDate();
+  }
+
+  compareIfExpired(now_date: Date, target_date: Date): boolean {
+    return dayjs(now_date).isAfter(target_date);
   }
 }

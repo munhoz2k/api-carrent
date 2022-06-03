814fa20ac5587558ce72eb5857a51c2ea965861b
Lucas Munhoz <lucasmunhozarruda@gmail.com>
2022-06-02 21:27:19 -0300

    Password Change Use Case

---
+++ src/modules/accounts/useCases/forgotPassword/ForgotPasswordUseCase.ts
@@ -0,0 +1,62 @@
+import { randomUUID } from "crypto";
+import { resolve } from "path";
+import { inject, injectable } from "tsyringe";
+
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
+import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
+import { IMailProvider } from "@shared/container/providers/MailProvider/IMailProvider";
+import { AppError } from "@shared/errors/AppError";
+
+@injectable()
+export class ForgotPasswordUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository,
+    @inject("UsersTokensRepository")
+    private usersTokensRepository: IUsersTokensRepository,
+    @inject("DayjsDateProvider")
+    private dayjsDateProvider: IDateProvider,
+    @inject("EtherealMailProvider")
+    private mailProvider: IMailProvider
+  ) {}
+
+  async execute(email: string) {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError("Users does not exists!", 404);
+    }
+
+    const refresh_token = randomUUID();
+
+    const expires_date = this.dayjsDateProvider.addHours(3);
+
+    await this.usersTokensRepository.create({
+      user_id: user.id,
+      refresh_token,
+      expires_date,
+    });
+
+    const templatePath = resolve(
+      __dirname,
+      "..",
+      "..",
+      "views",
+      "emails",
+      "forgotPassword.hbs"
+    );
+
+    const variables = {
+      name: user.name,
+      link: `${process.env.FORGOT_MAIL_URL}${refresh_token}`,
+    };
+
+    await this.mailProvider.sendMail(
+      email,
+      "Recuperação de senha RentX",
+      variables,
+      templatePath
+    );
+  }
+}

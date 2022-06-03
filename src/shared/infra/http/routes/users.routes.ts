dea77c480f944cd6b928d54ec2e97cfb03f0d3dc
Lucas Munhoz <lucasmunhozarruda@gmail.com>
2022-06-02 21:41:50 -0300

    User Routes

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -4,14 +4,18 @@ import multer from "multer";
 import uploadConfig from "@config/upload";
 import { CreateUserController } from "@modules/accounts/useCases/createUser/CreateUserController";
 import { UpdateUserAvatarController } from "@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
+import { UserProfileController } from "@modules/accounts/useCases/userProfile/UserProfileController";
 import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
 
 const usersRoutes = Router();
-const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"));
+
+const uploadAvatar = multer(uploadConfig);
 
 const createUserController = new CreateUserController();
 const updateUserAvatarController = new UpdateUserAvatarController();
+const userProfileController = new UserProfileController();
 
+usersRoutes.get("/profile", ensureAuthenticated, userProfileController.handle);
 usersRoutes.post("/", createUserController.handle);
 usersRoutes.patch(
   "/avatar",

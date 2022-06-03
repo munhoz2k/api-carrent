cc50172fd2d9cd294fcf6a7c7c003e6e5f15ac93
Lucas Munhoz <lucasmunhozarruda@gmail.com>
2022-06-02 21:41:57 -0300

    App File

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -2,9 +2,11 @@ import express, { NextFunction, Request, Response } from "express";
 import swaggerUi from "swagger-ui-express";
 
 import "reflect-metadata";
+import "dotenv/config";
 import "express-async-errors";
 import "@shared/container";
 
+import upload from "@config/upload";
 import { AppError } from "@shared/errors/AppError";
 import { router } from "@shared/infra/http/routes";
 import createConnection from "@shared/infra/typeorm";
@@ -18,6 +20,8 @@ app.use(express.json());
 createConnection();
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
+app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
 app.use(router);
 
